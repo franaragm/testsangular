3aaecdad78bf5214a84987b92cd4caf337699cca
franaragm <franaragonmesa@gmail.com>
2018-12-19 11:02:06 +0100

    configuarcion de pruebas de integración

--- src/app/intermedio2/medico/medico.component.spec.ts
+++ src/app/intermedio2/medico/medico.component.spec.ts
@@ -1,6 +1,26 @@
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {MedicoComponent} from './medico.component';
+
 describe('Medico Componente', () => {
 
+  let component: MedicoComponent;
+  let fixture: ComponentFixture<MedicoComponent>;
 
+  beforeEach(() => {
 
+    TestBed.configureTestingModule({
+      declarations: [MedicoComponent],
+      providers: [],
+      imports: []
+    });
+
+    fixture = TestBed.createComponent(MedicoComponent);
+    component = fixture.componentInstance;
+
+  });
+
+  it('', () => {
+
+  });
 
 });
