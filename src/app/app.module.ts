bbceb3fe4e9f41c23390843e537c2d42dbc500ad
franaragm <franaragonmesa@gmail.com>
2018-12-20 12:35:22 +0100

    configuarcion de pruebas de integración 275 276 277 278 279

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {MedicosComponent} from './intermedio/espias/medicos.component';
-import { MedicoComponent } from './intermedio2/medico/medico.component';
+import {MedicoComponent} from './intermedio2/medico/medico.component';
+import {HospitalComponent} from './intermedio2/hospital/hospital.component';
+import {IncrementadorComponent} from './intermedio2/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
-    MedicoComponent
+    MedicoComponent,
+    HospitalComponent,
+    IncrementadorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/intermedio2/incrementador/incrementador.component.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { IncrementadorComponent } from './incrementador.component';
+import { FormsModule } from '@angular/forms';
+import {By} from '@angular/platform-browser';
+
+
+describe('Incremendator Component', () => {
+
+    let component: IncrementadorComponent;
+    let fixture: ComponentFixture<IncrementadorComponent>;
+
+    beforeEach( () => {
+        TestBed.configureTestingModule({
+            declarations: [ IncrementadorComponent ],
+            imports: [ FormsModule ]
+        });
+
+        fixture = TestBed.createComponent(IncrementadorComponent);
+        component = fixture.componentInstance;
+
+    });
+
+    it('Debe de mostrar la leyenda', () => {
+
+      component.leyenda = 'Progreso de carga';
+
+      fixture.detectChanges(); // disparar la deteccion de cambios
+
+      const elem: HTMLElement = fixture.debugElement.query(By.css('h3')).nativeElement;
+
+      expect(elem.innerHTML).toContain('Progreso de carga');
+
+    });
+
+  it('Debe de mostrar en el input el valor del progreso', () => {
+
+    component.cambiarValor(5);
+    fixture.detectChanges();
+
+    // para evitar que no se evalue antes de que se efectuen los cambios en el dom
+    fixture.whenStable().then(() => {
+
+      const input = fixture.debugElement.query(By.css('input'));
+      const elem = input.nativeElement;
+
+      console.log(elem);
+
+      expect(elem.value).toBe('55');
+
+    });
+
+  });
+
+});
